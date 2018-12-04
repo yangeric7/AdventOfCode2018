4cd59f422e9320fb2ce27e0a8be6313f3af62c93
yangeric7 <yangeric7@gmail.com>
2018-12-03 21:23:48 -0600

    finished day3 part 2

--- solutions/day3/day3.py
+++ solutions/day3/day3.py
@@ -1,13 +1,5 @@
-#i really hate O(n^3) and i want to fix
-def part1(claim_array):
-    fabric = [[0]*1000 for _ in range(1000)]
-
-    for claim in claim_array:
-        id, left, top, width, height = get_claim(claim)
-        for i in range(left, left + width):
-            for j in range(top, top + height):
-                fabric[i][j] += 1
-    
+#how do i do this in a more pythonic way
+def part1(fabric):
     num_overlap = 0
     for i in range(len(fabric)):
         for j in range(len(fabric[i])):
@@ -16,11 +8,37 @@ def part1(claim_array):
 
     return num_overlap
 
-def main():
-    with open('input.txt', 'r') as input_file:
-        input = input_file.readlines()
+def part2(fabric, input_claim):
+    overlap = False
+
+    for claim in input_claim:
+        id, left, top, width, height = get_claim(claim)
+        for i in range(left, left + width):
+            for j in range(top, top + height):
+                if fabric[i][j] >= 2:
+                    overlap = True
+                    break
+            if overlap:
+                break
+        
+        if not overlap:
+            return id
+        else:
+            overlap = False
     
-    print('Part 1 Answer: ' + str(part1(input)))
+    return None
+
+#i hate O(n^3) -- probably more pythonic way maybe unavoidable for this
+def create_fabric(input_claim):
+    fabric = [[0]*1000 for _ in range(1000)]
+
+    for claim in input_claim:
+        _, left, top, width, height = get_claim(claim)
+        for i in range(left, left + width):
+            for j in range(top, top + height):
+                fabric[i][j] += 1
+            
+    return fabric
 
 def get_claim(raw_input):
     claims = raw_input.split(' ')
@@ -29,6 +47,16 @@ def get_claim(raw_input):
     [width, height] = claims[3].split('x')
 
     return int(id), int(left), int(top), int(width), int(height)
+
+
+def main():
+    with open('input.txt', 'r') as input_file:
+        input = input_file.readlines()
+    
+    fabric = create_fabric(input)
+
+    print('Part 1 Answer: ' + str(part1(fabric)))
+    print('Part 2 Answer: ' + str(part2(fabric, input)))
 
 if __name__ == '__main__':
     main()
