6d8658a1951abe4449b523cdf1baed34a6964fbd
yangeric7 <yangeric7@gmail.com>
2018-12-10 23:42:35 -0600

    day9 part2 finished. deque savior and hope elves are happy

--- solutions/day9/day9.py
+++ solutions/day9/day9.py
@@ -1,8 +1,12 @@
+from collections import deque
+
 input_statement = '468 players; last marble is worth 71010 points'
 
 num_players = 468
 last_marble = 71010
+last_marble_p2 = last_marble * 100
 
+# elves were definitely amused at this solution :3
 def part1():
     scores = [0 for i in range(num_players)]
     game = [0]
@@ -21,6 +25,24 @@ def part1():
 
         player_index = (player_index + 1) % num_players
         
-    return scores
+    return max(scores)
 
-print(max(part1()))
+def part2():
+    scores = [0 for i in range(num_players)]
+    game = deque([0])
+    player_index = 0
+
+    for i in range(1, last_marble_p2 + 1):
+        if i % 23 == 0:
+            game.rotate(-7)
+            scores[player_index] += (i + game.pop())
+        else:
+            game.rotate(2)
+            game.append(i)
+
+        player_index = (player_index + 1) % num_players
+
+    return max(scores)
+
+print(part1())
+print(part2())
