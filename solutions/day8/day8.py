0d7124e903045e3c0194182133edc49af0bc6c7a
yangeric7 <yangeric7@gmail.com>
2018-12-10 22:26:18 -0600

    finished day8 part2

--- solutions/day8/day8.py
+++ solutions/day8/day8.py
@@ -13,4 +13,20 @@ def part1(tree):
 
     return total_sum, tree[meta:]
 
+def part2(tree):
+    child, meta = tree[:2]
+    tree = tree[2:]
+    value = []
+
+    for _ in range(child):
+        child_value, tree = part2(tree)
+        value.append(child_value)
+
+    if child == 0:
+        return sum(tree[:meta]), tree[meta:]
+    else:
+        return sum([value[x-1] for x in tree[:meta] if x <= len(value) and x > 0]), tree[meta:]
+    
+
 print(part1(input_tree))
+print(part2(input_tree))
